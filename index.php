400578ed6928f465ee586e29bdbf741476fe4ba4
Rukky Ujara <rukkyujara@gmail.com>
2021-01-25 18:52:01 +0100

    More Files

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+//Config
+//require 'config.php';
+require 'config/paths.php';
+require 'config/database.php';
+require 'config/constants.php';
+
+// Also spl_autoload_register (Take a look at it if you like)
+
+// function __autoload($class) {
+//     require LIBS . $class . ".php";
+// }
+
+// function spl_autoload_register($class) {
+
+//     $path = 'libs/';
+
+
+//     require $path . $class . ".php";
+// }
+
+spl_autoload_register(function ($class) {
+    require 'libs/' . $class . '.php';
+});
+
+
+// Use an authloader !
+//require 'libs/Bootstrap.php';
+//require 'libs/Controller.php';
+//require 'libs/Model.php';
+//require 'libs/View.php';
+//Library
+//require 'libs/Database.php';
+//require 'libs/Session.php';
+//require 'libs/Hash.php';
+
+$app = new Bootstrap();
+?> 
